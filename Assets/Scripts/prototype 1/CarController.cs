ec50c5a4c3eeae7dcb75185c8d55e02a3d608de1
Dante Dardaque <dante.dardaque@hotmail.com>
2024-09-10 11:50:55 -0300

    car_physics
    car rigidbody = center of mass. local positon

--- Assets/Scripts/prototype 1/CarController.cs
+++ Assets/Scripts/prototype 1/CarController.cs
@@ -5,17 +5,28 @@ using UnityEngine.UIElements;
 
 public class CarController : MonoBehaviour
 {
-    private const float speed = 20.0f;
+    [SerializeField] private float horsePower;
     [SerializeField] private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody carRb;
+    [SerializeField] private GameObject centerOfMass;
+
+    private void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = centerOfMass.transform.localPosition;
+    }
 
     void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         // move the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // changed speed to horsePower
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        carRb.AddRelativeForce(Vector3.forward * Time.deltaTime * horsePower * forwardInput);
 
         // rotate the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);     
