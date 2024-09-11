efdcd32c2d7bae48e61b358f118051e90a6cf53d
Dante Dardaque <dante.dardaque@hotmail.com>
2024-09-11 18:15:59 -0300

    better_carPhysics

--- Assets/Scripts/prototype 1/CarController.cs
+++ Assets/Scripts/prototype 1/CarController.cs
@@ -6,19 +6,25 @@ using TMPro;
 
 public class CarController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    private float speed;
     [SerializeField] private float horsePower;
     [SerializeField] private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
     private Rigidbody carRb;
     [SerializeField] private GameObject centerOfMass;
-    [SerializeField] private TextMeshProUGUI speedMeterText; 
+    [SerializeField] private TextMeshProUGUI speedMeterText;
+    [SerializeField] private TextMeshProUGUI rpmText;
+    private float rpm;
+
+    [SerializeField] private List<WheelCollider> allWheels;
+    private int wheelsOnGround;
 
     private void Start()
     {
         carRb = GetComponent<Rigidbody>();
         carRb.centerOfMass = centerOfMass.transform.localPosition;
+
     }
 
     void FixedUpdate()
@@ -26,16 +32,47 @@ public class CarController : MonoBehaviour
         speed = Mathf.Round(carRb.velocity.magnitude * 3.6f); //3.6 for kph
         speedMeterText.SetText("Speed: " + speed + "Km/h");
 
+        rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.SetText("Rpm: " + rpm);
 
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        // move the car forward based on vertical input
-        // changed speed to horsePower
-        // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        if (isOnGround())
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            forwardInput = Input.GetAxis("Vertical");
 
-        carRb.AddRelativeForce(Vector3.forward * Time.deltaTime * horsePower * forwardInput);
+            if (speed <= 60)
+            {
+                carRb.AddRelativeForce(Vector3.forward * horsePower * Time.deltaTime * forwardInput);
+            }
 
-        // rotate the car based on horizontal input
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);     
+            if (speed > 0)
+            {
+                carRb.AddTorque(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+            }
+
+        }
+
+    }
+
+    bool isOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == 0)
+        {
+            return false;
+
+        } else
+        {
+            return true;
+        }
+
     }
 }
